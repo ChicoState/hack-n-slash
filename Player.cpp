63fb57675d966311330493ec4f2177b7ad87e056
Scott Backer <shpinxis@yahoo.com>
2015-09-13 20:00:21 -0700

    //Scott Backer 9/13/15 7:59 PM -- Add more to the player class

--- Player.cpp
+++ Player.cpp
@@ -1,48 +1,81 @@
-//
+//Scott Backer
+
+//Player Class CPP: Player class will handle all systems having to do with the character that the player controls. 
 
 #include "Player.h"
 
+//!The constructor for the player class
+//In - 
+//		int InputScreenWidth - the input screen width dimension of the game
+//		int INputScreenHeight - the input screen height dimension of the game
+//		ALLEGRO_EVENT_QUEUE* InputEventQueue - the overall game event queue input into the player class
 Player::Player(int InputScreenWidth, int InputScreenHeight, ALLEGRO_EVENT_QUEUE* InputEventQueue) : m_EventQueue(InputEventQueue)
 {
+	//Set input variables to member variables
 	m_ScreenWidth = InputScreenWidth;
 	m_ScreenHeight = InputScreenHeight;
 
+	//register the event sources to the event queue
 	al_register_event_source(m_EventQueue, al_get_keyboard_event_source());
 
-	m_PositionX = m_ScreenWidth / 2;
-	m_PositionY = m_ScreenHeight / 2;
-	m_KeyboardMap["W"] = false;
-	m_KeyboardMap["S"] = false;
-	m_KeyboardMap["A"] = false;
-	m_KeyboardMap["D"] = false;
+	//initialize the member variables
+	ClassTag = "Player";
+	m_XBound = 50;
+	m_YBound = 50;
+	m_XPosition = m_ScreenWidth / 2;
+	m_YPosition = m_ScreenHeight / 2;
+	m_KeyboardMap["W"] = false; //W used to move player up
+	m_KeyboardMap["S"] = false; //S used to move player down
+	m_KeyboardMap["A"] = false; //A used to move player left
+	m_KeyboardMap["D"] = false; //D used to move player right
 	m_MovementSpeed = 7;
+	m_CanMoveUp = true;
+	m_CanMoveDown = true;
+	m_CanMoveLeft = true;
+	m_CanMoveRight = true;
+	m_LockedXPosition = 0;
+	m_LockedYPosition = 0;
 }
 
+//!The deconstructor for the player class
 Player::~Player()
 {
 
 }
 
+//!Handles all the functions for the player that need to be called every update
 void Player::EventHandler()
 {
+	//start the wait for event sequence
 	al_wait_for_event(m_EventQueue, &m_AlEvent);
 
+	//check player movement
 	CheckMovement();
 
+	//draw the player if the right time
 	if(m_AlEvent.type = ALLEGRO_EVENT_TIMER)
 	{
 		DrawPlayer();
 	}
 }
 
+//!Draws the player character to the screen
 void Player::DrawPlayer()
 {
-	al_draw_filled_rectangle(m_PositionX, m_PositionY - 9, m_PositionX + 10, m_PositionY - 7, al_map_rgb(255, 0, 0));
-	al_draw_filled_rectangle(m_PositionX, m_PositionY + 9, m_PositionX + 10, m_PositionY + 7, al_map_rgb(255, 0, 0));
-	al_draw_filled_triangle(m_PositionX - 12, m_PositionY - 17, m_PositionX + 12, m_PositionY, m_PositionX - 12, m_PositionY + 17, al_map_rgb(0, 255, 0));
-	al_draw_filled_rectangle(m_PositionX - 12, m_PositionY - 2, m_PositionX + 15, m_PositionY + 2, al_map_rgb(0, 0, 255));
+	//draw the bound points
+	al_draw_pixel(GetXNorthBoundPoint(), GetYNorthBoundPoint(), al_map_rgb(255, 0, 0));
+	al_draw_pixel(GetXEastBoundPoint(), GetYEastBoundPoint(), al_map_rgb(255, 255, 255));
+	al_draw_pixel(GetXSouthBoundPoint(), GetYSouthBoundPoint(), al_map_rgb(0, 0, 255));
+	al_draw_pixel(GetXWestBoundPoint(), GetYWestBoundPoint(), al_map_rgb(0, 255, 0));
+	
+	//draw the character
+	al_draw_filled_rectangle(m_XPosition, m_YPosition - 9, m_XPosition + 10, m_YPosition - 7, al_map_rgb(255, 0, 0));
+	al_draw_filled_rectangle(m_XPosition, m_YPosition + 9, m_XPosition + 10, m_YPosition + 7, al_map_rgb(255, 0, 0));
+	al_draw_filled_triangle(m_XPosition - 12, m_YPosition - 17, m_XPosition + 12, m_YPosition, m_XPosition - 12, m_YPosition + 17, al_map_rgb(0, 255, 0));
+	al_draw_filled_rectangle(m_XPosition - 12, m_YPosition - 2, m_XPosition + 15, m_YPosition + 2, al_map_rgb(0, 0, 255));
 }
 
+//!Handles movement for the player character each update
 void Player::CheckMovement()
 {
 	/*
@@ -67,29 +100,43 @@ void Player::CheckMovement()
 	}
 	*/
 
+	//if the right time call functions
 	if(m_AlEvent.type == ALLEGRO_EVENT_TIMER)
 	{
 		if(m_KeyboardMap["W"])
 		{
-			MoveUp();
+			if(m_CanMoveUp)
+			{
+				MoveUp();
+			}
 		}
 
 		if(m_KeyboardMap["S"])
 		{
-			MoveDown();
+			if(m_CanMoveDown)
+			{
+				MoveDown();
+			}
 		}
 
 		if(m_KeyboardMap["A"])
 		{
-			MoveLeft();
+			if(m_CanMoveLeft)
+			{
+				MoveLeft();
+			}
 		}
 
 		if(m_KeyboardMap["D"])
 		{
-			MoveRight();
+			if(m_CanMoveRight)
+			{
+				MoveRight();
+			}
 		}
 	}
 
+	//if there is a key pressed down
 	else if(m_AlEvent.type == ALLEGRO_EVENT_KEY_DOWN)
 	{	
 		switch(m_AlEvent.keyboard.keycode)
@@ -112,8 +159,12 @@ void Player::CheckMovement()
 		}
 	}
 
+	//if there is a key unpressed
 	else if(m_AlEvent.type == ALLEGRO_EVENT_KEY_UP)
 	{
+		//test
+		printf("W Pressed = %i", m_KeyboardMap["W"]);
+		
 		switch(m_AlEvent.keyboard.keycode)
 		{
 		case ALLEGRO_KEY_W:
@@ -135,53 +186,183 @@ void Player::CheckMovement()
 	}
 }
 
+//!Moves the player negative in the y axis
 void Player::MoveUp()
 {
-	m_PositionY -= m_MovementSpeed;
-
-	if(m_PositionY < 0)
+	if(GetYNorthBoundPoint() < 0)
 	{
-		m_PositionY = 0;
+		m_LockedYPosition = m_YPosition;
+		m_YPosition = m_LockedYPosition;
+	}
+
+	else
+	{
+		m_YPosition -= m_MovementSpeed;
 	}
 }
 
+//!Moves the player positive in the y axis
 void Player::MoveDown()
 {
-	m_PositionY += m_MovementSpeed;
-
-	if(m_PositionY > m_ScreenHeight)
+	if(GetYSouthBoundPoint() > m_ScreenHeight)
 	{
-		m_PositionY = m_ScreenHeight;
+		m_LockedYPosition = m_YPosition;
+		m_YPosition = m_LockedYPosition;
+	}
+
+	else
+	{
+		m_YPosition += m_MovementSpeed;
 	}
 }
 
+//!Move sthe player negative in the x axis
 void Player::MoveLeft()
 {
-	m_PositionX -= m_MovementSpeed;
-
-	if(m_PositionX < 0)
+	if(GetXWestBoundPoint() < 0)
 	{
-		m_PositionX = 0;
+		m_LockedXPosition = m_XPosition;
+		m_XPosition = m_LockedXPosition;
+	}
+
+	else
+	{
+		m_XPosition -= m_MovementSpeed;
 	}
 }
 
+//!Moves the player positive in the x axis
 void Player::MoveRight()
 {
-	m_PositionX += m_MovementSpeed;
-
-	if(m_PositionX > m_ScreenWidth)
+	if(GetXEastBoundPoint() > m_ScreenWidth)
 	{
-		m_PositionX = m_ScreenWidth;
+		m_LockedXPosition = m_XPosition;
+		m_XPosition = m_LockedXPosition;
+	}
+
+	else
+	{
+		m_XPosition += m_MovementSpeed;
 	}
 }
 
-int Player::GetXPosition()
+//!Gets and returns the player class tag
+//Out - 
+//		string - the player class tag
+std::string Player::GetTag()
 {
-	return m_PositionX;
+	return ClassTag;
 }
 
+//!Gets and returns the player X Bound
+//Out - 
+//		int - player x bound
+int Player::GetXBound()
+{
+	return m_XBound;
+}
+
+//!Gets and returns the player Y Bound
+//Out - 
+//		int - player y bound
+int Player::GetYBound()
+{
+	return m_YBound;
+}
+
+//!Gets and returns the North Bound Point x position
+//Out - 
+//		int - North Bound Point x position
+int Player::GetXNorthBoundPoint()
+{
+	return m_XPosition;
+}
+
+//!Gets and returns the North Bound Point y position
+//Out - 
+//		int - North Bound Point y position
+int Player::GetYNorthBoundPoint()
+{
+	return (m_YPosition - (m_YBound / 2));
+}
+
+//!Gets and returns the South Bound Point x position
+//Out - 
+//		int - South Bound Point x position
+int Player::GetXSouthBoundPoint()
+{
+	return m_XPosition;
+}
+
+//!Gets and returns the South Bound Point y position
+//Out - 
+//		int - South Bound Point y position
+int Player::GetYSouthBoundPoint()
+{
+	return (m_YPosition + (m_YBound / 2));
+}
+
+//!Gets and returns the East Bound Point x position
+//Out - 
+//		int - East Bound Point x position
+int Player::GetXEastBoundPoint()
+{
+	return (m_XPosition + (m_XBound / 2));
+}
+
+//!Gets and returns the East Bound Point y position
+//Out - 
+//		int - East Bound Point y position
+int Player::GetYEastBoundPoint()
+{
+	return m_YPosition;
+}
+
+//!Gets and returns the West Bound Point x position
+//Out - 
+//		int - West Bound Point x position
+int Player::GetXWestBoundPoint()
+{
+	return (m_XPosition - (m_XBound / 2));
+}
+
+//!Gets and returns the West Bound Point y position
+//Out - 
+//		int - West Bound Point y position
+int Player::GetYWestBoundPoint()
+{
+	return m_YPosition;
+}
+
+//!Gets and returns the X position of the player
+//Out - 
+//		int - the current x position of the player
+int Player::GetXPosition()
+{
+	return m_XPosition;
+}
+
+//!Gets and returns the Y position of the player
+//Out - 
+//		int - the current y position of the player
 int Player::GetYPosition()
 {
-	return m_PositionY;
+	return m_YPosition;
+}
+
+//!Sets the x position of the player
+//In - 
+//		int NewXPosition - the new x position for the player
+void Player::SetXPosition(int NewXPosition)
+{
+	m_XPosition = NewXPosition;
+}
+
+//!Sets the y position of the player
+//In - 
+//		int NewYPosition - the new y position for the player
+void Player::SetYPosition(int NewYPosition)
+{
+	m_YPosition = NewYPosition;
 }
 
--- Player.h
+++ Player.h
@@ -1,4 +1,6 @@
-//
+//Scott Backer
+
+//Player Class H: Player class will handle all systems having to do with the character that the player controls. 
 
 #include <allegro5\allegro.h>
 #include <allegro5\allegro.h>
@@ -13,32 +15,55 @@ class Player
 {
 public:
 
-	Player(int InputScreenWidth, int InputScreenHeight, ALLEGRO_EVENT_QUEUE* InputEventQueue);
+	Player(int InputScreenWidth, int InputScreenHeight, ALLEGRO_EVENT_QUEUE* InputEventQueue); //Constructor for the player class
 
-	~Player();
+	~Player(); //Deconstructor for the player class
 
-	void EventHandler();
+	void EventHandler(); //Handles all the functions for the player that need to be called every update
 	
-	int GetXPosition();
-	int GetYPosition();
+	std::string GetTag(); //Gets and returns the player class tag
+	int GetXBound(); //Gets and returns the X Bound of the player
+	int GetYBound(); //Gets and returns the Y Bound of the player
+	int GetXNorthBoundPoint(); //!Gets and returns the North Bound Point x position
+	int GetYNorthBoundPoint(); //!Gets and returns the North Bound Point y position
+	int GetXSouthBoundPoint(); //!Gets and returns the South Bound Point x position
+	int GetYSouthBoundPoint(); //!Gets and returns the South Bound Point y position
+	int GetXEastBoundPoint(); //!Gets and returns the East Bound Point x position
+	int GetYEastBoundPoint(); //!Gets and returns the East Bound Point y position
+	int GetXWestBoundPoint(); //!Gets and returns the West Bound Point x position
+	int GetYWestBoundPoint(); //!Gets and returns the West Bound Point y position
+	int GetXPosition(); //Gets and returns the X position of the player
+	int GetYPosition(); //Gets and returns the Y position of the player
+
+	void SetXPosition(int NewXPosition); //Sets the x position of the player
+	void SetYPosition(int NewYPosition); //Sets the y position of the player
 
 private:
 
-	void DrawPlayer();
-	void CheckMovement();
-	void MoveUp();
-	void MoveDown();
-	void MoveLeft();
-	void MoveRight();
+	void DrawPlayer(); //Draws the player character to the screen
+	void CheckMovement(); //Handles movement for the player character each update
+	void MoveUp(); //Moves the player negative in the y axis
+	void MoveDown(); //Moves the player positive in the y axis
+	void MoveLeft(); //Move sthe player negative in the x axis
+	void MoveRight(); //Moves the player positive in the x axis
 
-	ALLEGRO_EVENT_QUEUE *m_EventQueue;
-	ALLEGRO_EVENT m_AlEvent;
+	ALLEGRO_EVENT_QUEUE *m_EventQueue; //The event queue for the player class
+	ALLEGRO_EVENT m_AlEvent; //the event variable for the player class
 
-	int m_ScreenWidth;
-	int m_ScreenHeight;
+	int m_ScreenWidth; //the screen width dimension of the game
+	int m_ScreenHeight; //the screen height dimension of the game
 
-	int m_PositionX;
-	int m_PositionY;
-	std::map <std::string, bool> m_KeyboardMap;
-	int m_MovementSpeed;
+	std::string ClassTag; //tag for the player class
+	int m_XBound; //the x bound for the player
+	int m_YBound; //the y bound for the player
+	int m_XPosition; //the current x position of the player
+	int m_YPosition; //the current y position of the player
+	std::map <std::string, bool> m_KeyboardMap; //map of the keyboard keys and their bools for the player
+	int m_MovementSpeed; //the speed at which the player moves
+	bool m_CanMoveUp; //true if the player can move up else false
+	bool m_CanMoveDown; //true if the player can move down else false
+	bool m_CanMoveLeft; //true if the player can move left else false
+	bool m_CanMoveRight; //true if the player can move right else false
+	int m_LockedXPosition; //x position to lock the player to when their bounds go off screen
+	int m_LockedYPosition; //y position to lock the player to when their bounds go off screen
 };
